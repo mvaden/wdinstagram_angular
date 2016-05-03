8f26a7d955e431e2927f844558b6ab5e7d956c9a
Matt <mvaden08@gmail.com>
2016-05-02 23:25:25 -0400

    Built app.js.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,25 @@
 "use strict";
 
 (function(){
-  angular.module("wdinstagram", [])
+  angular.module("wdinstagram", [
+    'ui.router',
+    'posts'
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ])
 })();
+
+function RouterFunction ($stateProvider){
+  .state("postsIndex", {
+    url: "/posts",
+    templateUrl: "js/grumbles/index.html"
+    controller: "GrumbleIndexController",
+    controllerAs: "PostIndexViewModel"
+  })
+  .state("postShow", {
+    url: "/posts/:id",
+    templateUrl: "js/posts/show.html"
+  })
+}
